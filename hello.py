f7e3328f31044915b90351739165c46bd8ced454
Sean MacGahan <smacgaha@gmail.com>
2017-02-25 12:18:28 -0500

    add POST /shock endpoint

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
+def shock_logic(magnitude):
+    return 'TODO'
+
+
+@app.route("/", methods=['GET'])
 def hello():
     return "Hello World!"
+
+
+@app.route("/shock", methods=['POST'])
+def shock():
+    payload = request.get_json()
+    magnitude = payload['magnitude']
+    shock_logic(magnitude)
+    return 'Shocked with magnitude of {}'.format(magnitude)
+
 
 if __name__ == "__main__":
     app.run()
